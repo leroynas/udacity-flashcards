435ee3f2dbb26e605fbd5bf3e70c3973c493cec7
Leroy Nas <leroynas119@gmail.com>
2019-10-15 10:17:19 +0200

    feat: added theme to project

---
+++ app/core/theme.js
@@ -0,0 +1,45 @@
+export default {
+  font: {
+    family: {
+      primary: 'lato-regular',
+      secondary: 'crete-round-regular',
+    },
+    size: {
+      large: 32,
+      medium: 24,
+      small: 16,
+    },
+  },
+  palette: {
+    primary: {
+      background: '#2d9cdb',
+      text: '#ffffff',
+    },
+    dark: {
+      background: '#363636',
+      text: '#ffffff',
+    },
+    light: {
+      background: '#ffffff',
+      text: '#363636',
+    },
+    status: {
+      green: {
+        background: '#6fcf97',
+        text: '#ffffff',
+      },
+      red: {
+        background: '#eb5757',
+        text: '#ffffff',
+      },
+    },
+  },
+  spacing: {
+    large: 30,
+    medium: 20,
+    small: 10,
+  },
+  shape: {
+    borderRadius: 14,
+  },
+};
