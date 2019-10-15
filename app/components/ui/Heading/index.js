3b21201496b5f096c03b54ffdc08e4ddcfd9a1d4
Leroy Nas <leroynas119@gmail.com>
2019-10-15 13:03:20 +0200

    feat: heading spacing based on props

--- app/components/ui/Heading/index.js
+++ app/components/ui/Heading/index.js
@@ -1,9 +1,18 @@
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Heading = styled.Text`
   font-family: ${props => props.theme.font.family.secondary};
   font-size: ${props => props.theme.font.size.large}px;
-  margin-bottom: ${props => props.theme.spacing.small}px;
+  margin-bottom: ${props => props.theme.spacing[props.spacing]}px;
 `;
+
+Heading.propTypes = {
+  spacing: PropTypes.string,
+};
+
+Heading.defaultProps = {
+  spacing: 'none',
+};
 
 export default Heading;
